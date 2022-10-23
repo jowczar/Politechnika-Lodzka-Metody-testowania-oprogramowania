7355e9bdd7ac0f8ae03b74a77e5808d358128cce
Justyna Owczarek <229301@edu.p.lodz.pl>
2022-10-23 10:33:57 +0200

    Refactor if from lab2 to a guard statement

--- lab3/main.c
+++ lab3/main.c
@@ -27,52 +27,52 @@ int my_printf(char *format_string, char *param){
 			continue;
 		}
 
+		// case after # we have some character other than k or .
+		if (format_string[i+1] != '.') {
+			putchar(format_string[i]);
+			continue;
+		}
+
 		// we have parameter specifier and length specifier 
 		// so we try to print the first n characters of the parameter
-		if (format_string[i+1] == '.') {
-			
-			// case we have nothing after the length specifier
-			if (i+2 >= strlen(format_string)) {
-				putchar(format_string[i]);
-				continue;
-			}
+		
+		// case we have nothing after the length specifier
+		if (i+2 >= strlen(format_string)) {
+			putchar(format_string[i]);
+			continue;
+		}
 
-			// case next is some character other than a number
-			if (format_string[i+2] < '0' || format_string[i+2] > '9') {
-				putchar(format_string[i]);
-				continue;
-			}
-			
-			// find the next non-number character
-			int next_i = i + 2; // #.[this character]
-			char next_char = format_string[next_i]; 
-			int length = 0;
-			while (next_char >= '0' && next_char <= '9' && next_i <= strlen(format_string)) {
-				length = (length * 10) + (next_char - '0');
-
-				next_i++;
-				next_char = format_string[next_i];
-			}
-
-			// case we have length specifier but the number is not followed by k
-			if (next_char != 'k') {
-				putchar(format_string[i]);
-				continue;
-			}
-			
-			// case we have length specifier and this number is followed by k (the correct case)
-			i = next_i;
-			swap_case(param);
-			for (int j=0; j<length; j++) {
-				if (j >= strlen(param)) break;
-				putchar(param[j]);
-			}
-
+		// case next is some character other than a number
+		if (format_string[i+2] < '0' || format_string[i+2] > '9') {
+			putchar(format_string[i]);
 			continue;
 		}
 		
-		// case after # we have some character other than k or .
-		putchar(format_string[i]);
+		// find the next non-number character
+		int next_i = i + 2; // #.[this character]
+		char next_char = format_string[next_i]; 
+		int length = 0;
+		while (next_char >= '0' && next_char <= '9' && next_i <= strlen(format_string)) {
+			length = (length * 10) + (next_char - '0');
+
+			next_i++;
+			next_char = format_string[next_i];
+		}
+
+		// case we have length specifier but the number is not followed by k
+		if (next_char != 'k') {
+			putchar(format_string[i]);
+			continue;
+		}
+		
+		// case we have length specifier and this number is followed by k (the correct case)
+		i = next_i;
+		swap_case(param);
+		for (int j=0; j<length; j++) {
+			if (j >= strlen(param)) break;
+			putchar(param[j]);
+		}
+		
 	}
 	puts("");
 	return 0;
