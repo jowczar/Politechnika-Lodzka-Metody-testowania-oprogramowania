6ebc8aa15b4eda73d984e0aca13a43502a40c0ce
Justyna Owczarek <229301@edu.p.lodz.pl>
2022-11-06 10:57:27 +0100

    Add reversing algorithm

--- lab4/main.js
+++ lab4/main.js
@@ -4,10 +4,22 @@ process.stdin.setEncoding('utf8');
 
 var lingeringLine = "";
 
+function handle_g_param(param) {
+	if (!Number(param)) {
+		// TODO; this is handled incorrectly, check how C handles this
+		console.log("Error: #g parameter is not a number");
+		// process.stdout.write(param);
+		return;
+	}
+
+	const reversedNumber = param.toString().split("").reverse().join("");
+	process.stdout.write(reversedNumber);
+}
+
 function my_printf(format_string,param){
 	for(var i=0;i<format_string.length;i++){
 		if((format_string.charAt(i) == '#') && (format_string.charAt(i+1) == 'g')){
-			process.stdout.write(param);
+			handle_g_param(param);
 			i++;
 		}else{
 			process.stdout.write(format_string.charAt(i));
