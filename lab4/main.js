77e677d73b45e97ef191672d1c07b30791dd8a47
Justyna Owczarek <229301@edu.p.lodz.pl>
2022-11-06 11:32:14 +0100

    Change console error to simple text so tester can handle it

--- lab4/main.js
+++ lab4/main.js
@@ -6,7 +6,7 @@ var lingeringLine = "";
 
 function handle_g_param(param) {
 	// what if too big number for int?
-	
+
 	if (!Number(param)) {
 		throw new Error(`Error: parameter ${param} is not a valid number`);
 	}
@@ -16,6 +16,10 @@ function handle_g_param(param) {
 }
 
 function my_printf(format_string,param){
+	if (!Number(param)) {
+		throw new Error(`Error: parameter ${param} is not a valid number`);
+	}
+
 	for(var i=0;i<format_string.length;i++){
 		if((format_string.charAt(i) == '#') && (format_string.charAt(i+1) == 'g')){
 			handle_g_param(param);
@@ -33,7 +37,12 @@ process.stdin.on('data', function(chunk) {
 	lines[0] = lingeringLine + lines[0];
 	lingeringLine = lines.pop();
 	for(var i=0;i<lines.length;i++){
-		my_printf(lines[i],lines[i+1])
+		try {
+			my_printf(lines[i],lines[i+1])
+		} catch (err) {
+			// hide error and just print 'Error' instead so the C tester can handle it
+			process.stdout.write("Error\n");
+		}
 		i++;
 	}
 
