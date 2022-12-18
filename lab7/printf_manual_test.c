3f1616b4bb8eab12756b13f637bc0b2f6cbfcb6c
Justyna Owczarek <229301@edu.p.lodz.pl>
2022-12-18 10:39:38 +0100

    Modify tests for unsigned integer range

--- lab7/printf_manual_test.c
+++ lab7/printf_manual_test.c
@@ -2,6 +2,10 @@
 
 int main(void)
 {
-  printf("text with%.10-something", 123);
+  printf("In C, x stands for unsigned integer represented as lowercase heximal number\n");
+  printf("text with %x signed integer has undefined behaviour, it will be handled as Error\n", -15);
+  printf("In C unsigned int has range 0 to 4,294,967,295\n");
+  printf("text with %x normal integer range + 1 prints OK\n", 2147483648);
+  printf("text with %x unsigned integer range + 1 prints overflows, it will be handled as Error\n",  4294967296);
   return 0;
 }
