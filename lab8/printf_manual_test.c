1a752276dead39867cb13207d2557c86b09f89fd
Justyna Owczarek <229301@edu.p.lodz.pl>
2023-01-15 10:40:29 +0100

    Add manual tests

--- lab8/printf_manual_test.c
+++ lab8/printf_manual_test.c
@@ -5,5 +5,8 @@ int main(void)
   printf("In C, x stands for unsigned integer represented as lowercase heximal number\n");
   printf("===========================================\n");
   printf("text with %.3x\n", 200000);
+  printf("text with %.1x\n", 1234);
+  printf("text with %.1x\n", 2147483645);
+  
   return 0;
 }
